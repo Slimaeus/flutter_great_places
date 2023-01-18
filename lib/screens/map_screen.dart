dfb04a795b16202d905ddc4bc4ace8b6ba07e4a2
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-01-18 09:34:41 +0700

    Allow user to pick location

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -13,17 +13,38 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedLocation;
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Map'),
+        actions: [
+          if (widget.isSelecting)
+            IconButton(
+                onPressed: _pickedLocation == null
+                    ? null
+                    : () {
+                        Navigator.of(context).pop(_pickedLocation);
+                      },
+                icon: Icon(Icons.check))
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
             target: LatLng(widget.initialLocation.latitude,
                 widget.initialLocation.longitude),
             zoom: 16),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? null
+            : {Marker(markerId: MarkerId('m1'), position: _pickedLocation)},
       ),
     );
   }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_great_places/helpers/location_helper.dart';
 import 'package:flutter_great_places/screens/map_screen.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
@@ -21,14 +22,16 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(MaterialPageRoute(
-        fullscreenDialog: true,
-        builder: (ctx) => MapScreen(
-              isSelecting: true,
-            )));
+    final selectedLocation =
+        await Navigator.of(context).push<LatLng>(MaterialPageRoute(
+            fullscreenDialog: true,
+            builder: (ctx) => MapScreen(
+                  isSelecting: true,
+                )));
     if (selectedLocation == null) {
       return;
     }
+    //...
   }
 
   @override
